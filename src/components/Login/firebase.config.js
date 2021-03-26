77c9a5d8940b7585b6ae537b02a1ffbfe5cfe26a
Asfakur <saikat.hap@gmail.com>
2021-03-26 18:04:19 +0600

    47-1 Module introduction and project setup

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDnWMm-jwFPyabwaY-pe5U7y3EN8UrilRU",
+  authDomain: "burj-al-arab-a01ea.firebaseapp.com",
+  projectId: "burj-al-arab-a01ea",
+  storageBucket: "burj-al-arab-a01ea.appspot.com",
+  messagingSenderId: "240744306112",
+  appId: "1:240744306112:web:ed24c87057e8d5f0416425"
+};
+
+export default firebaseConfig;
